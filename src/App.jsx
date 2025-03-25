23a01da5b46a153135704af35ab031210e203391
AliAtherAyyubi <info2atherayyubi@gmail.com>
2025-03-25 21:53:08 +0500

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
 import './App.css'
-
+// import { Calendar } from "@/components/ui/calendar"
+import { IconCloudDemo } from './components/iconcloud'
+import { LensDemo } from './components/lens'
+// import { Globe } from "@/components/magicui/globe";
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     <div className='text-2xl text-center font-bold text-red-400 '>
+    <div className='text-4xl text-center font-bold text-pink-400 '>
       Shadcn Practice
      </div>
+    <div className='flex justify-center'>
+    <IconCloudDemo/>
+
+    </div>
+
+    <LensDemo/>
+     
+     
+    {/* <GlobeDemo/> */}
+    {/* <Calendar
+      mode="single"
+      className="rounded-md border w-2xs"
+    /> */}
     </>
   )
 }
---
+++ src/components/iconcloud.jsx
@@ -0,0 +1,39 @@
+import { IconCloud } from "@/components/magicui/icon-cloud";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+
+  "express",
+
+  "amazonaws",
+
+  "firebase",
+
+  "docker",
+  "git",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "figma",
+];
+
+export function IconCloudDemo() {
+  const images = slugs.map(
+    (slug) => `https://cdn.simpleicons.org/${slug}/${slug}`
+  );
+
+  return (
+    <div className="relative flex size-full items-center justify-center overflow-hidden">
+      <IconCloud images={images} />
+    </div>
+  );
+}
